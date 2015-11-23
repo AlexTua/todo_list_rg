62dfea63f926963214bd2be515daeadf1993387e
AlexTua <taler97@gmail.com>
2015-11-23 22:16:02 +0200

    Add rename project

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -2,7 +2,7 @@ class UiController < ApplicationController
   before_action :signed_in_user
 
   def index
-  	@projects=current_user.projects.all;
+  	  @projects=current_user.projects.all;
   end
 
   def create 
@@ -16,5 +16,19 @@ class UiController < ApplicationController
   		redirect_to root_url
   	end
   end
+
+  def edit
+    @list=current_user.projects.find(params[:id])
+      respond_to do |format|
+        format.js
+      end
+  end
   
+  def update
+    @list=current_user.projects.find(params[:id])
+    if @list.update_attributes(:name => params[:name])
+ 
+    end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :ui, only: [:index, :create, :destroy]
+  resources :ui, only: [:index, :create, :destroy, :edit, :update]
 
   root  'ui#index'	
-  match '/signup',  		to: 'users#new',            via: 'get'
+  match '/signup',  		  to: 'users#new',             via: 'get'
 
-  match '/signin',  		to: 'sessions#new',         via: 'get'
-  match '/signout', 		to: 'sessions#destroy',     via: 'delete'
+  match '/signin',  	   	to: 'sessions#new',          via: 'get'
+  match '/signout', 	   	to: 'sessions#destroy',      via: 'delete'
 
-  match 'ui/create',		to: 'ui#create',			via: 'post'
-  match 'ui/destroy/:id',	to: 'ui#destroy',			via: 'delete'
+  match 'ui/create',		  to: 'ui#create',			       via: 'post'
+  match 'ui/destroy/:id',	to: 'ui#destroy',		       	 via: 'delete'
+  match 'ui/edit/:id',		to: 'ui#edit',			     	   via: 'get'
+  match 'ui/update/:id',  to: 'ui#update',             via: 'patch'
 end
