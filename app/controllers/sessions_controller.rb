e864e4991468bf043cdd00554d9e541bf629c873
AlexTua <taler97@gmail.com>
2015-11-30 23:23:48 +0200

    fix formatting 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 			sign_in(user)
 			redirect_to root_url
 		else
-      		render 'new'
+      render 'new'
 		end
 	end
 
--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -8,54 +8,54 @@ class UiController < ApplicationController
   end
 
   def create 
-  	if current_user.projects.create!(name: "New list")
+    if current_user.projects.create!(name: "New list")
       @project=current_user.projects.first
       @tasks=current_user.tasks.all
       @task_new=current_user.tasks.new
-  		  respond_to do |format|
-          format.js
-        end
+      respond_to do |format|
+        format.js
+      end
     else
-        respond_to do |format|
-          format.js {render 'errors/error'}
-        end
+      respond_to do |format|
+        format.js {render 'errors/error'}
+      end
     end
   end
 
   def destroy
-  	if current_user.projects.find(params[:id]).destroy
-  		  respond_to do |format|
-          format.js
-        end
+    if current_user.projects.find(params[:id]).destroy
+      respond_to do |format|
+        format.js
+      end
     else
-        respond_to do |format|
-          format.js {render 'errors/error'}
-        end
+      respond_to do |format|
+        format.js {render 'errors/error'}
+      end
     end
   end
 
   def edit
     if @project=current_user.projects.find(params[:id])
-        respond_to do |format|
-          format.js
-        end
+      respond_to do |format|
+        format.js
+      end
     else
-        respond_to do |format|
-          format.js {render 'errors/error'}
-        end
+      respond_to do |format|
+        format.js {render 'errors/error'}
+      end
     end
   end
   
   def update
     @project=current_user.projects.find(params[:id])
     if @project.update_attributes(:name => params[:name])
-        respond_to do |format|
-          format.js
-        end
+      respond_to do |format|
+        format.js
+      end
     else
-        respond_to do |format|
-          format.js {render 'errors/prj_error'}
-        end
+      respond_to do |format|
+        format.js {render 'errors/prj_error'}
+      end
     end
   end
 end
