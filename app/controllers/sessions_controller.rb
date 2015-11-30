01a1610358570562a1092654af4e36722d60f86c
AlexTua <taler97@gmail.com>
2015-11-30 23:29:23 +0200

    last fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
 class SessionsController < ApplicationController
-	def new
-		redirect_to root_url if signed_in?
-	end
+  def new
+    redirect_to root_url if signed_in?
+  end
 
-	def create
-		user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
-			sign_in(user)
-			redirect_to root_url
-		else
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in(user)
+      redirect_to root_url
+    else
       render 'new'
-		end
-	end
+    end
+  end
 
-	def destroy 
-		sign_out
-		redirect_to signin_path
-	end 
+  def destroy 
+    sign_out
+    redirect_to signin_path
+  end 
 end
